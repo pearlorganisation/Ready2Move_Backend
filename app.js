06df9c307239c9e571e200435b8f43c334132418
Ranjan <ranjan@pearlorganisation.com>
2025-03-13 12:10:39 +0530

    minor changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "development"
-        ? ["http://localhost:3002", "http://localhost:3001"]
+        ? ["http://localhost:3002", "http://localhost:3001","http://localhost:3000"]
         : ["*"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Specify allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
